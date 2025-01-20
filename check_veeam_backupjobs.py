af1f5592aa45f1ab1de495f37b6ed12e87cfaf57
offroadaaron <offroadaaron@gmail.com>
2025-01-20 23:13:41 +1100

    Update check_veeam_backupjobs.py

--- check_veeam_backupjobs.py
+++ check_veeam_backupjobs.py
@@ -64,14 +64,14 @@ def main():
 
     failed_jobs = []
     successful_jobs = 0
-    warning_jobs = 0
+    warning_jobs = []
     for job in jobs_states['data']:
         job_name = job['name']
         if job['lastRun'] is not None and datetime.strptime(job['lastRun'], '%Y-%m-%dT%H:%M:%S.%f%z').replace(tzinfo=None) > datetime.now() - timedelta(hours=args.max_backup_age):
             if job['lastResult'] == 'Success':
                 successful_jobs += 1
             elif job['lastResult'] == 'Warning':
-                warning_jobs += 1
+                warning_jobs.append(job)
             elif job['lastResult'] == 'Failed':
                 if args.job_filter is None or args.job_filter.lower() in job_name.lower():
                     failed_jobs.append(job)
@@ -80,17 +80,22 @@ def main():
         print(f"CRITICAL: Failed jobs within the allowed age range:")
         for job in failed_jobs:
             print(f"  - {job['name']}")
+        if warning_jobs:
+            print(f"Warning jobs: {len(warning_jobs)}")
+            for job in warning_jobs:
+                print(f"  - {job['name']}")
         print(f"Successful jobs: {successful_jobs}")
-        print(f"Warning jobs: {warning_jobs}")
         sys.exit(2)
-    else:
-        print(f"OK: No failed jobs found within the allowed age of {args.max_backup_age} hours")
+    elif warning_jobs:
+        print(f"WARNING: Warning jobs within the allowed age range:")
+        for job in warning_jobs:
+            print(f"  - {job['name']}")
         print(f"Successful jobs: {successful_jobs}")
-        print(f"Warning jobs: {warning_jobs}")
-        if warning_jobs > 0:
-            sys.exit(1)
-        else:
-            sys.exit(0)
+        sys.exit(1)
+    else:
+        print(f"OK: No failed or warning jobs found within the allowed age of {args.max_backup_age} hours")
+        print(f"Successful jobs: {successful_jobs}")
+        sys.exit(0)
 
 if __name__ == '__main__':
     main()
